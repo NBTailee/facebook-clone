c713481bcd744b5970791445814f7d59396dff23
NBTailee <87483288+NBTailee@users.noreply.github.com>
2022-06-30 10:22:59 +0700

    Update RightBar.js

--- src/components/RightBar.js
+++ src/components/RightBar.js
@@ -73,7 +73,7 @@ function RightBar() {
                     height: "100px",
                     margin: "0 20px 12px 0px",
                   }}
-                  src="https://scontent.fsgn2-2.fna.fbcdn.net/v/t45.1600-4/284459826_6309700022245_2233701949910647693_n.jpg?stp=cp0_dst-jpg_p960x960_q75_spS444&_nc_cat=103&ccb=1-7&_nc_sid=67cdda&_nc_ohc=OJ7LyBOz7osAX8-PEaG&_nc_ht=scontent.fsgn2-2.fna&oh=00_AT8d78afLVJM2M912B6rtOOnteeFV11icgMuH572EtxMkA&oe=62B74D4E"
+                  src="../public/img/str17.png"
                 />
               </ListItemAvatar>
               <ListItemText secondary="shope.ee" />
@@ -87,7 +87,7 @@ function RightBar() {
                     height: "100px",
                     margin: "0 20px 12px 0px",
                   }}
-                  src="https://scontent.fsgn2-6.fna.fbcdn.net/v/t45.1600-4/280393505_6281212806712_7437566226223747604_n.jpg?stp=c96.0.630.630a_cp0_dst-jpg_q75_spS444&_nc_cat=111&ccb=1-7&_nc_sid=67cdda&_nc_ohc=nE7G9sl5cJYAX_7tDQH&_nc_ht=scontent.fsgn2-6.fna&oh=00_AT-7eDQ7vxYXyaoFVcVv3y3eKAv6d6HoLT9bDgZH6H9rBg&oe=62B7D62D"
+                  src="../public/img/str16.jpg"
                 />
               </ListItemAvatar>
               <ListItemText primary="Xgear" secondary="thenewxgear.com" />
@@ -180,7 +180,7 @@ function RightBar() {
                     horizontal: "right",
                   }}
                 >
-                  <Avatar src="https://scontent.fsgn2-5.fna.fbcdn.net/v/t39.30808-1/269913141_398365365408767_3910118793640672214_n.jpg?stp=dst-jpg_p200x200&_nc_cat=102&ccb=1-7&_nc_sid=7206a8&_nc_ohc=UNYyTf_98G4AX_n5pCy&_nc_ht=scontent.fsgn2-5.fna&oh=00_AT-kao57qj7Qzyq0_fyPUmtxNkqzJJIoy90EZJ58LFlZ4w&oe=62B9701F" />
+                  <Avatar src="../public/img/str9.jpg" />
                 </Badge>
               </ListItemAvatar>
               <ListItemText primary="quyền cu" />
@@ -214,7 +214,7 @@ function RightBar() {
                     horizontal: "right",
                   }}
                 >
-                  <Avatar src="https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-6/275288485_501693098201800_6877783614788937850_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=hmlDor80uDEAX-E4g8k&_nc_ht=scontent.fsgn2-6.fna&oh=00_AT-K6sy5xRrOGH5xjoFds460A42s4TC-_GHbFR6QK7tYOg&oe=62B88645" />
+                  <Avatar src="../public/img/str15.jpg" />
                 </Badge>
               </ListItemAvatar>
               <ListItemText primary="Bắc nô lệ châu Phi" />
@@ -231,7 +231,7 @@ function RightBar() {
                     horizontal: "right",
                   }}
                 >
-                  <Avatar src="https://scontent.fsgn2-5.fna.fbcdn.net/v/t39.30808-6/271241978_998485581069891_6652549594922500611_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=xRi95JFtZ8gAX-2F3JO&_nc_ht=scontent.fsgn2-5.fna&oh=00_AT95tjGLR92BMPQ9mbrNbMBVS0Ume9XMZ9OIj6_jCM5mrQ&oe=62B8C0E6" />
+                  <Avatar src="../public/img/str7.jpg" />
                 </Badge>
               </ListItemAvatar>
               <ListItemText primary="Vũ cu bé" />
@@ -265,7 +265,7 @@ function RightBar() {
                     horizontal: "right",
                   }}
                 >
-                  <Avatar src="https://scontent.fsgn2-4.fna.fbcdn.net/v/t39.30808-1/283313028_1163417694451660_1711388925889115451_n.jpg?stp=dst-jpg_s200x200&_nc_cat=101&ccb=1-7&_nc_sid=7206a8&_nc_ohc=5sJj9_u04g0AX_ZuatR&tn=c7RL3FztuUPaO1x5&_nc_ht=scontent.fsgn2-4.fna&oh=00_AT-2-ybSE3L27F1T4DUg_cwsKCd38tdyh_tHKpEbuOg5NA&oe=62B894D1" />
+                  <Avatar src="../public/img/str6.jpg"/>
                 </Badge>
               </ListItemAvatar>
               <ListItemText primary="Lộc wibu retard" />
@@ -282,7 +282,7 @@ function RightBar() {
                     horizontal: "right",
                   }}
                 >
-                  <Avatar src="https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-1/229703776_1268658543565831_2518618770794049158_n.jpg?stp=dst-jpg_p100x100&_nc_cat=110&ccb=1-7&_nc_sid=7206a8&_nc_ohc=sUXf_ZgN9W8AX89UtwH&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fsgn2-6.fna&oh=00_AT_ZOfrbwZ7ZJT5HNlv0CI9mQqreMFQ42QlLGz0eYdxO1A&oe=62B96121" />
+                  <Avatar src="../public/img/str8.jpg" />
                 </Badge>
               </ListItemAvatar>
               <ListItemText primary="Fuoc liên kết" />
@@ -316,7 +316,7 @@ function RightBar() {
                     horizontal: "right",
                   }}
                 >
-                  <Avatar src="https://scontent.fsgn2-4.fna.fbcdn.net/v/t39.30808-1/284634976_378956194269330_7463322868103999610_n.jpg?stp=dst-jpg_p100x100&_nc_cat=109&ccb=1-7&_nc_sid=7206a8&_nc_ohc=hxLHJfCBrTAAX-l0rko&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fsgn2-4.fna&oh=00_AT9yDpYAtGxKhQbX2x6x2i21UCRaW_YM49wftRPJndglyQ&oe=62B7E80D" />
+                  <Avatar src="../public/img/str10.jpg" />
                 </Badge>
               </ListItemAvatar>
               <ListItemText primary="Thiên lỏd" />
